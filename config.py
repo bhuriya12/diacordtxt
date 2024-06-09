660a1ef6e81f534c450aaa2680697903f0c13216
Abhishek <126752231+abhishek82089@users.noreply.github.com>
2024-06-08 22:57:46 -0700

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-api_id = 17864183
-api_hash = "b7cf3765dcf2e2781c98fc57f9f87a5b"
-bot_token = "6752335890:AAH0Dav1-QtqmsMLiaM2ho9MEcpuTRJSNdg"
-auth_users = "6629551092, 1318247204"
+api_id = 27862729
+api_hash = "1d244f3bb5c5b746d4b9444b48d3508b"
+bot_token = "6813417286:AAH134ybixPBDPco_zhIRyuu6VXj71Aw8tE"
+auth_users = "1782965867, 1318247204"
 sudo_users = [int(num) for num in auth_users.split(",")]
-osowner_users = "6629551092"
+osowner_users = "1782965867"
 owner_users = [int(num) for num in osowner_users.split(",")]
