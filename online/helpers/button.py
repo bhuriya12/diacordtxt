c28a168f46ffabe681983afeb80e593b3f4854c1
Abhishek <126752231+abhishek82089@users.noreply.github.com>
2024-06-08 23:13:38 -0700

    Update button.py

--- online/helpers/button.py
+++ online/helpers/button.py
@@ -5,11 +5,11 @@ keyboard = InlineKeyboardMarkup(
         [
             InlineKeyboardButton(
                 text="Developer",
-                url="https://t.me/LegendBot_Owner",
+                url="https://t.me/teamshauryabot",
             ),
             InlineKeyboardButton(
                 text="About Me",
-                url="https://t.me/LegendBot_AI/476",
+                url="https://t.me/teamshaurya01",
             ),
         ],
     ]
