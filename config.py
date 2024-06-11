76151b23cbd3b49545030580311d1e106ac5ef10
Abhishek <126752231+abhishek82089@users.noreply.github.com>
2024-06-11 18:57:00 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 api_id = 27862729
 api_hash = "1d244f3bb5c5b746d4b9444b48d3508b"
 bot_token = "6813417286:AAH134ybixPBDPco_zhIRyuu6VXj71Aw8tE"
-auth_users = "1782965867,  6840586526,  5483260471,  6770619899,  6830450483"
+auth_users = "1782965867,  6770619899,  6830450483"
 sudo_users = [int(num) for num in auth_users.split(",")]
 osowner_users = "1782965867"
 owner_users = [int(num) for num in osowner_users.split(",")]
