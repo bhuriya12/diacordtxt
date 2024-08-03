843357e966ee9e8d34a4a79fa3dfa804f198315f
bhuriya12 <162349127+bhuriya12@users.noreply.github.com>
2024-08-03 23:37:04 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-api_id = 27862729
-api_hash = "1d244f3bb5c5b746d4b9444b48d3508b"
-bot_token = "6813417286:AAH134ybixPBDPco_zhIRyuu6VXj71Aw8tE"
-auth_users = "1782965867,  6770619899,  1259551035"
+api_id = 26451206
+api_hash = "32984406271d6f3945bb536671b143a7"
+bot_token = "7014953988:AAFIp-jmpXFQnwLE7M9ItyIQDGa8RLNFNh0"
+auth_users = "6830450483"
 sudo_users = [int(num) for num in auth_users.split(",")]
-osowner_users = "1782965867"
+osowner_users = "6830450483"
 owner_users = [int(num) for num in osowner_users.split(",")]
